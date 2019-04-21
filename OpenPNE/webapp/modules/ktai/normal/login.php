c7b0018f2ae5aa0146fbda24a8b5cf38718b354f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-21 18:20:29 +0900

    #12

--- OpenPNE/webapp/modules/ktai/normal/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-function normalAction_login($smarty,$requests)
-{
-	$smarty->ext_display('login.tpl');
-}
-
--- html/ktai_page.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once './config.inc.php';
-require_once DOCUMENT_ROOT . '/lib/init.inc';
-
-module_execute('ktai', 'page', $_REQUEST['p']);
-
